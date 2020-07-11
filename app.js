acba50554446a3eb797d4dd25492d6a9f133cc5e
Shawn Huggins <shawnhuggins@Shawns-MBP.lan>
2020-07-11 09:47:55 +0900

    Add sqlite db intergration.

--- app.js
+++ app.js
@@ -2,36 +2,46 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var sqlite3 = require('sqlite3');
+var sqlite = require('sqlite');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+sqlite.open({
+  filename: path.join(__dirname, 'postal_codes.db'),
+  driver: sqlite3.Database
+}).then((db) => {
+  app.use(function (req, res, next) {
+    req.db = db;
+    next();
+  });
+  app.use('/', indexRouter);
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+}).catch((err) => {
+  console.log(err)
+  process.exit(1)
+})
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  try {
+    count = await req.db.get("SELECT COUNT(*) as rows FROM postal_codes")
+    console.log(count)
+    res.render('index', { title: 'Express' });
+  } catch(err) {
+    next(err)
+  }
 });
 
 module.exports = router;
