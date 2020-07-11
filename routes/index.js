04bbded905bb6b8783b59a970ecd5cb846cd718a
Shawn Huggins <shawnhuggins@Shawns-MBP.lan>
2020-07-11 11:07:37 +0900

    Add postal code search

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,17 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   try {
-    count = await req.db.get("SELECT COUNT(*) as rows FROM postal_codes")
-    console.log(count)
-    res.render('index', { title: 'Express' });
+    postalCodeString = req.query.postal_code
+    if (postalCodeString == null) {
+      res.render('index', { title: 'Search for a Japan Postal Code' });
+      return
+    }
+    postalCode = await req.db.get('SELECT * FROM postal_codes WHERE postal_code = ?', postalCodeString)
+    if (postalCode) {
+      res.render('postal_code', {postalCode: postalCode})
+    } else {
+      res.render('not_found', { searchedPostalCode: postalCodeString });
+    }
   } catch(err) {
     next(err)
   }
