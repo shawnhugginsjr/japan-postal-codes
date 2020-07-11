fc6490461a637ed373112b86e37a2c71209f1635
Shawn Huggins <shawnhuggins@Shawns-MBP.lan>
2020-07-11 22:34:35 +0900

    Add fetching of weather and near postal codes

---
+++ network.js
@@ -0,0 +1,26 @@
+var axios = require('axios');
+
+network = {
+    fetchPostcodeData: (postalCode) => {
+        noHyphenPostalCode = postalCode.split('-').join('')
+        url = 'https://apis.postcode-jp.com/api/v3/postcodes/' + noHyphenPostalCode
+        console.log(url)
+        return axios.get(url, {
+            headers: {
+                "apikey": "GVgQlNR36mSJGxUwe96756DEb2VXRZ0Sncd4ZD3"
+            },
+        })
+    },
+    fetchWeatherData: (lat, lon) => {
+        appid = '4b91afdb742d3a52e4363b6731ef7708'
+        return axios.get("https://api.openweathermap.org/data/2.5/onecall", {
+            params: {
+                lat: lat,
+                lon: lon,
+                appid: "4b91afdb742d3a52e4363b6731ef7708"
+            },
+        })
+    }
+}
+
+module.exports = network;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var network = require('../network')
 
-/* GET home page. */
+postalCodeRegex = /\d\d\d-\d\d\d\d/
+postalCodeLength = 8
+sqlNearPostalCodes = "SELECT child.* FROM postal_codes as child, (SELECT id FROM postal_codes WHERE postal_code = ?) as parent WHERE child.id BETWEEN parent.id-2 AND parent.id+2;"
+
+
 router.get('/', async (req, res, next) => {
   try {
     postalCodeString = req.query.postal_code
@@ -9,13 +14,48 @@ router.get('/', async (req, res, next) => {
       res.render('index', { title: 'Search for a Japan Postal Code' });
       return
     }
-    postalCode = await req.db.get('SELECT * FROM postal_codes WHERE postal_code = ?', postalCodeString)
-    if (postalCode) {
-      res.render('postal_code', {postalCode: postalCode})
-    } else {
-      res.render('not_found', { searchedPostalCode: postalCodeString });
+
+    if (postalCodeString.length !== postalCodeLength || postalCodeString.search(postalCodeRegex) === -1) {
+      res.render('postal_code', {
+        searchedPostalCode: postalCodeString,
+        error: `'${postalCodeString}' is an invalid Postal Code.}`
+      })
+      return
     }
-  } catch(err) {
+
+    postCodeData = await network.fetchPostcodeData(postalCodeString)
+    if (postCodeData.status === 204) {
+      res.render('postal_code', {
+        searchedPostalCode: postalCodeString,
+        error: `Postal Code '${postalCodeString}' could not be found.`
+      })
+    }
+
+    lat = postCodeData.data.location.latitude
+    lon = postCodeData.data.location.longitude
+    weatherPromise = network.fetchWeatherData(lat, lon)
+    nearPostalCodesPromise = req.db.all(sqlNearPostalCodes, postalCodeString)
+
+    Promise.allSettled([weatherPromise, nearPostalCodesPromise]).then((values) => {
+      weatherData = (values[0].status === "fulfilled" ? values[0].value.data.daily.slice(0, 3) : null)
+      nearPostalCodes = (values[1].status === "fulfilled" ? values[1].value : null)
+      if (nearPostalCodes) {
+        mainPostalCode = nearPostalCodes.find(code => code.postal_code === postalCodeString)
+      } else {
+        // LOAD mainPostalCode from PostCodeData
+      }
+      res.render('postal_code', {
+        weatherData: weatherData,
+        nearPostalCodes: nearPostalCodes,
+        mainPostalCode: mainPostalCode,
+        searchedPostalCode: postalCodeString,
+        location: {
+          lat: lat,
+          lon: lon
+        }
+      })
+    })
+  } catch (err) {
     next(err)
   }
 });
